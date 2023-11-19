5bb69f342e1a13a9cdef76525251341bf5762cf5
Jahangir Alam <ja3560124@gmail.com>
2023-11-19 18:50:53 +0600

    SignalRWithGroupChating

---
+++ WebApplication1/Hubs/MessageHub.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication1.Hubs
+{
+    public class MessageHub:Hub
+    {
+        private static Dictionary<string, string> users = new Dictionary<string, string>();
+        public static List<string> groups = new List<string> { "C#", "SQL", "R52" };
+        public async override Task OnConnectedAsync()
+        {
+            await Clients.Caller.SendAsync("connected", "You are connected");
+        }
+        public async Task AddMe(string name)
+        {
+
+            users.Add(Context.ConnectionId, name);
+            await Clients.Caller.SendAsync("Added", "You are added");
+            await Clients.All.SendAsync("grouplist", groups);
+
+        }
+        public async Task Join(string group)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, group);
+            await Clients.All.SendAsync("userlist", users.Values.ToArray());
+        }
+        public async Task Create(string group)
+        {
+            groups.Add(group);
+            await Clients.All.SendAsync("grouplist", groups);
+        }
+        public async Task Send(string group, string msg)
+        {
+            await Clients.Groups(group).SendAsync("recieveMessage", users[Context.ConnectionId], msg);
+        }
+        public async override Task OnDisconnectedAsync(Exception? exception)
+        {
+
+            users.Remove(Context.ConnectionId);
+            await Task.CompletedTask;
+        }
+    }
+}
